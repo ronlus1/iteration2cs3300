2d38b76d8d55d402588861a0888a02dfaf0c5bd3
Ron Lustig <ronlustig111@gmail.com>
2023-04-24 20:07:36 +0000

    message

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_20_183223) do
+ActiveRecord::Schema.define(version: 2023_04_22_203511) do
 
   create_table "addcustomers", force: :cascade do |t|
     t.string "name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2023_04_20_183223) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
