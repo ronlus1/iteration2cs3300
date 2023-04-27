f4f94400936043ad937dee5f1d7316df54834c0b
Ron Lustig <ronlustig111@gmail.com>
2023-04-27 21:15:23 +0000

    jdjjd

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -84,6 +84,6 @@ SimpleCov.start 'rails' do
   add_filter '/app/models/adduser.rb'
   add_filter '/app/mailers/'
   add_filter '/app/models/application_record.rb'
-  add_filter '/app/models/user.rb'
+ # add_filter '/app/models/user.rb'
   
 end
